33d34de7f369ab689519b0e03e2aabc7eb033be3
CostanzaS <35370335+CostanzaS@users.noreply.github.com>
2018-09-11 13:37:33 +0200

    Add files via upload

---
+++ Rules.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class Rules{
+  Stone current = new Stone();
+  /*final static int BLACK = 1;
+  final static int WHITE = 2;
+  final static int EMPTY = 0;
+  final static int OFFBOARD = -1;*/
+  int playerId;
+  int stonesId;
+  Arraylist<Stone> stonesToBeSwapped = new ArrayList<>();
+  Arraylist<Stone> possiblePos = new ArrayList<>();
+  boolean valid;
+
+
+public boolean check(/*board, Stone stones and Stone new_stone*/){
+
+  for (i=-1 ; i<1 ; i++){
+    for (j=-1 ; j<1 ; j++){
+      if (!stone.getState().equals(current.getState()) || stone.getState() == EMPTY)){
+        int pos = (i,j);
+        if (pos == (-i,-j)|| pos == (-i,j) || pos == (-i,j+1)||pos == (i,-j) || pos == (i,j+1) || pos == (-i,-j) || pos==(i+1,j)||pos==(i+1,j+1)){
+              possiblepos.add(pos);
+              stonesToBeSwapped.add(stone);
+        }
+        valid = true;
+      }else{
+        valid = false;
+      }
+      return valid;
+    }
+  }
+  return valid;
+}
+
+public void flip(/*Stone stone*/){
+  if (current.getState().equals(BLACK)){
+    current.setState(WHITE) ;
+  }else if(current.getState().equals(WHITE)){
+    current.setState(BLACK);
+  }else if (current.getState().equals(EMPTY)){
+    if (playerId = 1){
+      current.setState(BLACK);
+    }else{
+      current.setState(WHITE);
+    }
+  }else{
+    System.out.println("OFFBOARD");
+  }
+}
+
+public boolean move(/*board , current stone, stones*/){
+  check();
+  if(valid == true){
+    for all stones in stonesToBeSwapped
+    flip();
+    
+  }
+}
+
+
+
+}
