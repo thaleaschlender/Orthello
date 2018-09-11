69572f9d04826da913d92b2eb50705c535605171
thaleaschlender <thalea.schlender@gmail.com>
2018-09-11 13:22:10 +0200

    final start

---
+++ src/Board.java
@@ -0,0 +1,26 @@
+public class Board {
+    Piece[][] board;
+
+    public Board(){
+        board = new Piece[8][8];
+        for(int i = 0; i < board.length; i++)
+            for(int j = 0; j < board[0].length; j++)
+                board[i][j] = new Piece(0);
+
+
+        board[3][3].changeColour(2);
+        board[3][4].changeColour(1);
+        board[4][3].changeColour(1);
+        board[4][4].changeColour(2);
+    }
+    public  void print (){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                System.out.print(board[i][j].getColour());
+                //System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/Piece.java
@@ -0,0 +1,15 @@
+public class Piece {
+    private int colour;
+
+    public Piece (int colour){
+        this.colour = colour;
+    }
+
+    public void changeColour (int colour){
+        this.colour = colour;
+    }
+    public int getColour (){
+        return colour;
+    }
+
+}
